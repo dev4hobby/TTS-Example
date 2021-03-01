044fac61cb8ea244f0c1f84230a8bbc9c0fa0be2
Seongchuel <aciddust20@gmail.com>
2021-03-01 18:17:52 +0900

    Update, settings

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 misc = {
     'lang': 'ko',
     'pitch': 2.0,
-    'random_factor': 0.25,
+    'random_factor': 0.20,
     'normal_frame_rate': 44100,
-    'skip_whitespace_level': 3  # if value high, getting faster
+    'skip_whitespace_level': 2  # if value high, getting faster
 }
